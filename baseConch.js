4060dd2496157a77984bf63f81f48b526bbb6cc9
Thomas Woerdeman <twoerdem@avans.nl>
2017-07-18 02:13:31 +0200

    A few code comments. Initialize develop branch

--- baseConch.js
+++ baseConch.js
@@ -3,20 +3,19 @@ const discord = require('Discord.js');
 const client = new discord.Client();
 const token = undefined;
 
-// -- CHECK IF TOKEN IS SET -- //
+  // -- CHECK IF TOKEN IS SET -- //
 token == undefined ? console.log('Fill in you discord token') : client.login(token);
 
   // -- INITIALIZE DATABASE CLASS -- //
 const dbManager = require('./databaseManager.js');
 const db = new dbManager();
 
-
-
+  // -- NOTIFY THE SERVER THAT ITS READY FOR USE -- //
 client.on('ready', () => {
-    console.log('I am ready sempai');
+    console.log('The Conch is ready. Fire away!');
 });
 
+  // -- DISCORD.JS LiSTENER FOR INCOMMING MESSAGES -- //
 client.on('message', (msg) => {
 
-
-})
+});
