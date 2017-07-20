3092b8bc2ef42f17f19e151611c48aa15faa5893
itsHowlingWolf <thomaswoerdeman@gmail.com>
2017-07-21 00:11:36 +0200

    Rebuilt database structure. Now has the functionality to save an yt url to the database

--- baseConch.js
+++ baseConch.js
@@ -1,21 +1,41 @@
   // -- INITIALIZE DISCORD -- //
 const discord = require('Discord.js');
 const client = new discord.Client();
-const token = undefined;
-
+const token = "MzM2NjI5OTEzNTI2MjA2NDc0.DE_3MA.b7AXtQXjQPxJEH4_T99wm-m8FZU";
   // -- CHECK IF TOKEN IS SET -- //
 token == undefined ? console.log('Fill in you discord token') : client.login(token);
-
   // -- INITIALIZE DATABASE CLASS -- //
 const dbManager = require('./databaseManager.js');
 const db = new dbManager();
-
   // -- NOTIFY THE SERVER THAT ITS READY FOR USE -- //
 client.on('ready', () => {
     console.log('The Conch is ready. Fire away!');
 });
-
   // -- DISCORD.JS LiSTENER FOR INCOMMING MESSAGES -- //
 client.on('message', (msg) => {
-
+    // check IF incomming messages start with a dollar sign
+    if(msg.content.startsWith('$')){
+         var tag = (msg.content.split(' ')[0]).replace("$","");
+         db.getAllTagsByName(tag).then(function(e){
+         });
+       }else if(msg.content.startsWith('#')){
+         var tag = (msg.content.split(' ')[0]).replace("#","");
+         var type = (msg.content.split(' ')[1]);
+         var url = (msg.content.split(' ')[2]);
+          if(type == 'yt'){
+            handleYoutube(tag, url);
+          }
+       }
 });
+
+function handleYoutube(tag, url){
+    db.saveYoutubeURL(url).then(function(e){
+      console.log(typeof e);
+      var yt_id = e["last_insert_rowid()"];
+      db.handleTag(tag, null, yt_id);
+    });
+}
+
+function handleImage(tag, url){
+
+}
--- databaseManager.js
+++ databaseManager.js
@@ -1,20 +1,37 @@
 var sqlite3 = require('sqlite3').verbose();
-var file = "database";
+var file = "database.sqlite3";
 var db = new sqlite3.Database(file);
 
 db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY AUTOINCREMENT, tag TEXT)");
-    var query = db.prepare("INSERT INTO tags VALUES (?,?)");
-    query.run(null,"Test");
+    db.run("CREATE TABLE IF NOT EXISTS images(id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS youtube_sounds(id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)")
+    db.run("CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY AUTOINCREMENT, tag TEXT, image_id INTEGER, yt_id INTEGER, FOREIGN KEY(image_id) REFERENCES images(id), FOREIGN KEY(yt_id) REFERENCES youtube_sounds(id))");
 
 });
 
 class dbManager{
-   getTags(){
-    db.each("SELECT id, tag FROM tags", function(err, row){
-        console.log(row.id + ": " + row.tag);
-    });
-  };
+  getAllTagsByName(name){
+      return db.each("SELECT id, tag FROM tags", function(err, row){
+        res.push(row);
+      });
+    };
+    handleTag(tag, image_id, yt_id){
+      var query = db.prepare("REPLACE INTO tags(tag, image_id, yt_id) VALUES (?,?,?)");
+      return new Promise(function(resolve, reject) {
+        query.run(tag, image_id, yt_id);
+      });
+    }
+
+    saveYoutubeURL(url){
+      var query = db.prepare("INSERT INTO youtube_sounds VALUES (?,?)");
+
+      return new Promise(function(resolve, reject){
+          query.run(null, url);
+          db.get("SELECT last_insert_rowid();", function(err, res){
+            resolve(res);
+          });
+        });
+    }
 };
 
 module.exports = dbManager;
